22a5108cd9a31201debeb1216ac2dec4f1d8ae90
Ayush-Manware <ayushmanware19@gmail.com>
2023-11-29 15:19:13 +0530

    Some Routes Created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const { data} = require("./data")
+const app = express()
+const cors = require("cors")
+
+app.use(cors())
+
+app.get("/api/main/mensClothing", (req, res)=>{
+    const filter = data.filter((a)=> a.category === "men's clothing")
+    res.send(filter)
+})
+
+app.get("/", (req, res)=>{
+    const filter = data.filter((a)=> a.category === "jewelery")
+    res.send(filter)
+})
+
+app.listen(2100,()=>{
+    console.log("Server Started")
+})
