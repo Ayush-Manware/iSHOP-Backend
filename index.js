68ffd94b5c3471a42b5e789cfd5d85af29602161
Ayush-Manware <ayushmanware19@gmail.com>
2023-12-04 14:54:54 +0530

    Sending Data Completed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 require('./database/config')
 const User = require("./database/user")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res)=>{
     res.send("Home")
